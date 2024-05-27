9f2b82b3397d3d1f685ea82a785e182907c83944
Jeff Eaton <jeff@eaton.fyi>
2024-05-27 13:09:27 -0500

    Roughing out a zod schema that covers base 11ty templates.

---
+++ src/schemas/markdown-post.ts
@@ -0,0 +1,53 @@
+import { z } from "zod";
+
+/**
+ * Optional todos:
+ * 
+ * - [ ] Auto-populate slug from title if it doesn't exist
+ * - [ ] Auto-populate date from filename if it doesn't exist
+ * - [ ] Auto-populate filename from date + slugified title
+ * - [ ] Auto-populate ID from ids and type
+ */
+
+export const EleventyNavigationSchema = z.object({
+  key: z.string().optional(),
+  title: z.string().optional(),
+  excerpt: z.string().optional(),
+  parent: z.string().optional(),
+  order: z.number().optional(),
+  url: z.string().url().optional(),  
+}).describe("Metadata to control Eleventy's nav + breadcrumb plugin.")
+
+export const FrontmatterSchema = z.object({
+  id: z.string().optional(), // IDs should follow a format like: `type/unique-key`
+  date: z.coerce.date().optional(),
+
+  ids: z.record(z.string()).optional().describe('Additional strings that identify the item. ISBN, Drupal node ID, etc.'),
+  dates: z.record(z.coerce.date()).optional().describe('Significant dates for the item. Created, Modified, Published, Copyright, etc.'),
+
+  slug: z.string().optional(),
+  permalink: z.string().or(z.boolean()).optional().describe("The internal path of the item on the site; if FALSE no page is generated."),
+  url: z.string().url().optional().describe("The item's canonical offsite URL, if one exists."),
+  archivedAt: z.string().url().optional().describe("The item's archival URL (usually archive.org)"),
+
+  // Actual content-like metadata
+  title: z.string().optional(),
+  summary: z.string().optional(),
+  headline: z.string().optional(),
+  dek: z.string().optional(),
+  excerpt: z.string().optional(),
+
+   // We use this instead of 'tags' because 11ty auto-generates collections for 'tags'
+  keywords: z.array(z.string()).optional(),
+
+  layout: z.string().optional(),
+  eleventyNavigation: EleventyNavigationSchema.optional(),
+});
+
+export const MarkdownPostSchema = z.object({
+  file: z.string().optional(),
+  data: FrontmatterSchema.optional(),
+  content: z.string().optional()
+});
+
+export type MarkdownPost = z.infer<typeof MarkdownPostSchema>;
