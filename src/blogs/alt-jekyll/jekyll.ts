ea26587f990fe2fc8468a4de9b1d8c1ca511b39c
Jeff Eaton <jeff@eaton.fyi>
2024-06-03 14:32:03 -0500

    Comment cleanup in aisle Disqus

--- src/blogs/alt-jekyll/jekyll.ts
+++ src/blogs/alt-jekyll/jekyll.ts
@@ -1,12 +1,12 @@
 import { autop, toMarkdown } from '@eatonfyi/html';
 import { nanohash } from '@eatonfyi/ids';
 import { Frontmatter } from '@eatonfyi/serializers';
-import { CommentInput, CommentSchema } from '../../schemas/comment.js';
+import { Comment, CommentSchema } from '../../schemas/comment.js';
 import {
+  CreativeWork,
   CreativeWorkInput,
   CreativeWorkSchema,
 } from '../../schemas/creative-work.js';
-import { type MarkdownPost } from '../../schemas/markdown-post.js';
 import { BlogMigrator, BlogMigratorOptions } from '../blog-migrator.js';
 import * as Disqus from '../disqus-export.js';
 import { jekyllPostSchema, type JekyllPost } from './schema.js';
@@ -21,7 +21,10 @@ const defaults: BlogMigratorOptions = {
   output: 'src/entries/alt-jekyll',
 };
 
-export class AltJekyllMigrator extends BlogMigrator<MarkdownPost> {
+export class AltJekyllMigrator extends BlogMigrator {
+  entries: CreativeWork[] = [];
+  comments: Record<string, Comment[]> = {};
+
   constructor(options: BlogMigratorOptions = {}) {
     super({ ...defaults, ...options });
   }
@@ -31,7 +34,6 @@ export class AltJekyllMigrator extends BlogMigrator<MarkdownPost> {
   }
 
   override async readCache() {
-    const entries: JekyllPost[] = [];
     const files = this.input.find({ matching: '*.md' });
     for (const file of files) {
       this.log.debug(`Parsing ${file}`);
@@ -42,23 +44,62 @@ export class AltJekyllMigrator extends BlogMigrator<MarkdownPost> {
         )
         .then(result => {
           if (result.success) {
-            entries.push(result.data);
+            const cw = this.prepEntry(result.data);
+            this.entries.push(cw);
           } else {
             this.log.error(result.error.message);
           }
         });
     }
 
-    const comments: Disqus.Thread[] = [];
     for (const file of this.input.find({ matching: 'disqus*.xml' })) {
       const xml = this.input.read(file, 'utf8') ?? '';
-      comments.push(...(await Disqus.parse(xml)).threads);
+      const threads = (await Disqus.parse(xml)).threads;
+      for (const thread of threads) {
+        const identifier = thread.id;
+        this.comments[identifier] ??= [];
+        for (const comment of thread.posts) {
+          this.comments[identifier].push(this.prepComment(comment));
+        }
+      }
     }
 
-    return Promise.resolve({ entries, comments });
+    return Promise.resolve({ entries: this.entries, comments: this.comments });
   }
 
-  protected prepEntry(input: JekyllPost) {
+  override async finalize() {
+    const data = await this.readCache();
+    const commentStore = this.data.bucket('comments');
+
+    for (const e of data.entries) {
+      const { text, ...frontmatter } = e;
+      const fileName = this.toFilename(frontmatter);
+      this.output.write(fileName, { data: frontmatter, content: text });
+      this.log.debug(`Wrote ${fileName}`);
+
+      // Find a thread that matches this file.
+      const comments = this.comments[e.id];
+      if (comments.length) {
+        commentStore.set(frontmatter.id, comments);
+        this.log.debug(
+          `Saved ${comments.length} comments for ${frontmatter.url}`,
+        );
+      }
+    }
+
+    this.data.bucket('sources').set('alt-jekyll', {
+      id: 'alt-jekyll',
+      url: 'https://angrylittletree.com',
+      title: 'Angry Little Tree',
+      hosting: 'Github Pages',
+      software: 'Jekyll',
+    });
+
+    await this.copyAssets('files', 'alt');
+    return Promise.resolve();
+  }
+
+  protected prepEntry(input: JekyllPost): CreativeWork {
     const md: CreativeWorkInput = {
       id: 'tmp',
       date: input.data.date,
@@ -74,7 +115,7 @@ export class AltJekyllMigrator extends BlogMigrator<MarkdownPost> {
       md.slug ??= slug;
     }
     md.url = `http://angrylittletree.com/${input.data.date?.getFullYear()}/${input.data.date?.getUTCMonth()}/${input.data.slug}.html`;
-    md.id = nanohash(md.url);
+    md.id = 'alt-' + nanohash(md.url);
     md.isPartOf = 'alt-jekyll';
 
     md.content = input.content;
@@ -82,47 +123,15 @@ export class AltJekyllMigrator extends BlogMigrator<MarkdownPost> {
     return CreativeWorkSchema.parse(md);
   }
 
-  override async finalize() {
-    const data = await this.readCache();
-    const commentStore = this.data.bucket('comments');
-
-    for (const e of data.entries) {
-      const { content, ...frontmatter } = this.prepEntry(e);
-      const fileName = this.toFilename(frontmatter);
-      this.output.write(fileName, { data: frontmatter, content });
-      this.log.debug(`Wrote ${fileName}`);
-
-      // Find a thread that matches this file.
-      const comments =
-        data.comments.find(t => t.id === frontmatter.id)?.posts ?? [];
-      const mapped = comments.map(c => {
-        const comment: CommentInput = {
-          id: `altj-c${c.dsqId}`,
-          parent: c.parent ? `altj-c${c.parent}` : undefined,
-          sort: c.sort,
-          date: c.createdAt,
-          commenter: { name: c.author.name },
-          text: toMarkdown(autop(c.message)),
-        };
-        return CommentSchema.parse(comment);
-      });
-      if (mapped.length) {
-        commentStore.set(frontmatter.id, mapped);
-        this.log.debug(
-          `Saved ${mapped.length} comments for ${frontmatter.url}`,
-        );
-      }
-    }
-
-    this.data.bucket('sources').set('alt-jekyll', {
-      id: 'alt-jekyll',
-      url: 'https://angrylittletree.com',
-      title: 'Angry Little Tree',
-      hosting: 'Github Pages',
-      software: 'Jekyll',
+  protected prepComment(comment: Disqus.Post): Comment {
+    return CommentSchema.parse({
+      id: `altj-c${comment.dsqId}`,
+      parent: comment.parent ? `altj-c${comment.parent}` : undefined,
+      about: comment.linkId ? `altj-c${comment.linkId}` : undefined,
+      sort: comment.sort,
+      date: comment.createdAt,
+      commenter: { name: comment.author.name },
+      text: toMarkdown(autop(comment.message)),
     });
-
-    await this.copyAssets('files', 'alt');
-    return Promise.resolve();
   }
 }
