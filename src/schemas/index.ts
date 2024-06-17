a069575eb2f59a4462b6f82d767ba7ab553f6da2
Jeff Eaton <jeff@eaton.fyi>
2024-06-17 00:07:29 -0500

    Re-upping the schema index

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,11 +4,16 @@ export * from './person.js';
 export * from './place.js';
 export * from './thing.js';
 
+export * from './article.js';
 export * from './book.js';
+export * from './bookmark.js';
 export * from './comment.js';
 export * from './creative-work.js';
+export * from './episode.js';
 export * from './message.js';
 export * from './talk.js';
 
-export * from './fragments/dimensions.js';
-export * from './fragments/eleventy-navigation.js';
+export * from './role.js';
+export * from './social-media-post.js';
+
+export * from './fragments/index.js';
