e075d16013d7dc3249f5a68374cb845f867ba9d3
Jeff Eaton <jeff@eaton.fyi>
2024-06-17 00:12:04 -0500

    Adding optional attendee count for Events; we'll use this later for some dataviz

--- src/schemas/event.ts
+++ src/schemas/event.ts
@@ -6,6 +6,7 @@ export const EventSchema = ThingSchema.extend({
   type: z.string().default('Event'),
   dates: recordWithHints(z.coerce.date(), ['start', 'end']).optional(),
   places: z.record(z.string()).optional(),
+  attendees: z.coerce.number().optional(),
   isPartOf: oneOrMany(z.string()).optional(), // none, one, or more string or string/order objects
   hasPart: oneOrMany(z.string()).optional(), // none, one, or more string or string/order objects
 });
