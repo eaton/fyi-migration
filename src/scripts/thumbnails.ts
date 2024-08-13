b5ea62c60b41247733c2e2beb4e9f5825f296ffa
Jeff Eaton <jeff@eaton.fyi>
2024-08-12 19:34:51 -0500

    Really horrible generator shim

---
+++ src/scripts/thumbnails.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import jetpack from '@eatonfyi/fs-jetpack';
+import SimpleThumbnail from 'simple-thumbnail-ts';
+// @ts-expect-error "Default export workaround"
+const SimpleThumbnailClass = SimpleThumbnail.default;
+
+const assets = jetpack.dir(process.env.MIGRATION_ROOT ?? '').dir(process.env.MIGRATION_ASSETS ?? '');
+
+const thumb = new SimpleThumbnailClass();
+for (const video of assets.find({ matching: '**/*.mp4' })) {
+  try {
+    await thumb.generate(
+      assets.path(video),
+      assets.path(video) + '.jpeg',
+      '450x?',
+      {}
+    )
+    console.log('Done!');
+  } catch (err) {
+    console.error(err);
+  }
+}
